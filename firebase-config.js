a6d3fc210064694986c5059d4b5b1d401a80ee74
Gabriel Scapinello <gabrielsacpinello09@gmail.com>
2025-09-15 22:51:39 -0300

    feito o import do firebase authentication

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app"; // Importa a função para inicializar o app Firebase
+import { getAuth } from "firebase/auth"; // Importa as funções do Firebase Authentication
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyArntNoWXAlL9gzWdP_iWc7pGXhaEvSu3A",
   authDomain: "bistro-cetec-2025.firebaseapp.com",
